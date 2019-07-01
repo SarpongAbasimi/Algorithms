3ac193e479a68efab1a5ca8afabbfbc807770297
sarpong <abasimisarpong@rocketmail.com>
2019-07-01 21:21:36 +0100

    test for reverse algorithm passes

--- algoenv/algorithms/tests/algorithms_test.py
+++ algoenv/algorithms/tests/algorithms_test.py
@@ -13,6 +13,8 @@ class TestAlgorithms(object):
 
   def test_reverse(self, algorithm):
     assert algorithm.reverse_([1, 5]) == [5, 1]
+    assert algorithm.reverse_([2, 10, 6, 7, 9, 4]) == [4, 9, 7, 6, 10, 2]
   
   def test_that_it_returns_list_if_passed_string(self, algorithm):
     assert algorithm.reverse_('s')  == []
+
