1bbcc71b4de10616a3c9a00db8a4f837821dbaa7
sarpong <abasimisarpong@rocketmail.com>
2019-07-01 21:17:39 +0100

    test for reverse algorithm passes

--- algoenv/algorithms/tests/algorithms_test.py
+++ algoenv/algorithms/tests/algorithms_test.py
@@ -1,6 +1,18 @@
-import sys, os 
+import sys, os, pytest
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path + '/../')
 
 from lib.algorithms import Algorithms
 
+@pytest.fixture
+def algorithm():
+  algorithm = Algorithms()
+  return algorithm
+
+class TestAlgorithms(object):
+
+  def test_reverse(self, algorithm):
+    assert algorithm.reverse_([1, 5]) == [5, 1]
+  
+  def test_that_it_returns_list_if_passed_string(self, algorithm):
+    assert algorithm.reverse_('s')  == []
